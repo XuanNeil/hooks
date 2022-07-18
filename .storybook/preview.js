a2e69a1b12c7d7211c5fa26daa916c9e144d75c1
Dev Ha Xuan <xuanhv@reactplus.jp>
2022-07-18 18:05:25 +0700

    add theme chakra into storybook, config alias for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
+import { theme } from "../src/theme";
+
+// config theme chakra for storybook
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+   actions: { argTypesRegex: "^on[A-Z].*" },
+   controls: {
+      matchers: {
+         color: /(background|color)$/i,
+         date: /Date$/,
+      },
+   },
+   chakra: {
+      theme: theme,
+   },
+};
