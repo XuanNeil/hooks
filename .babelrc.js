61ccdf1387922e29a79740a88bc197b3f495c437
Dev Ha Xuan <xuanhv@reactplus.jp>
2022-07-18 18:03:36 +0700

    config alias with customize cra and babel-plugin-module-resolver

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+//https://www.npmjs.com/package/babel-plugin-module-resolver
+
+module.exports = {
+  "plugins": [
+    ["module-resolver", {
+      "root": ["./"],
+      "alias": {
+        "@src": "./src"
+      }
+    }]
+  ]
+}
