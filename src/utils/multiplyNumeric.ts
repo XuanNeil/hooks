51676bdc3e66639b26398beabddda016da988931
Dev Ha Xuan <xuanhv@reactplus.jp>
2022-07-28 11:51:10 +0700

    fix lint multiplyNumeric function

--- src/utils/multiplyNumeric.ts
+++ src/utils/multiplyNumeric.ts
@@ -1,11 +1,18 @@
-function multiplyNumeric(obj: any, count?: number) {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function multiplyNumeric(obj: any, count?: number) {
    if (!count) {
       count = 1;
    }
+
    for (const key in obj) {
       if (typeof obj[key] === "number") {
          obj[key] *= count;
       }
    }
+
    return obj;
 }
+
+// example
+multiplyNumeric({ firstName: "Neil", age: 20 }, 2);
