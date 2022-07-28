ab291d6cc952257829af592738cf84e676e8a8a7
Dev Ha Xuan <xuanhv@reactplus.jp>
2022-07-28 22:39:25 +0700

    fix lint isEmptyObject function

--- src/utils/isEmptyObject.ts
+++ src/utils/isEmptyObject.ts
@@ -4,3 +4,11 @@ export const isEmpty2yObject = (object: any) => {
    if (typeof object !== "object") return;
    return !!Object.keys(object).length;
 };
+
+const person = {
+   firstName: "Ha",
+   lastName: "Xuan",
+   age: 20,
+};
+
+isEmpty2yObject(person); // true
